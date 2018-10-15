386372d0b63648858372754f8470b4455f5f3433
luisherreratw <lherrera@thoughtworks.com>
2018-10-15 12:22:58 -0500

    adds condition for check that jenkins_home directory exists

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-mdkir $(pwd)/jenkins_home
+if [ -d $(pwd)/jenkins_home ]
+then
+    echo "Directory jenkins_home exists."
+else
+    echo "Creating the directory jenkins_home."
+    mkdir $(pwd)/jenkins_home
+fi
+
 docker build -t machine/jenkins-lts .
 docker run --name=server-jenkins -p 8080:8080 -p 50000:50000 -v $(pwd)/jenkins_home:/var/jenkins_home -v $(pwd):/home/DevOps --restart=always machine/jenkins-lts
